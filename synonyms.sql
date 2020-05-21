a8445859a2fbb4409aba370d4de8d1f259339271
kicbar <bartlomiejkicior@gmail.com>
2020-05-21 19:29:52 +0200

    add synonyms

---
+++ synonyms.sql
@@ -0,0 +1,27 @@
+    /*
+	Synonimy do tabel.
+    ------------------------------------------------------------------------------------------------------
+    VERSION      DATE         AUTHOR     DESCRIPTION
+    1.0.21/05    21/05/2020   b.kicior   Utworzenie synonimów. 
+    ------------------------------------------------------------------------------------------------------
+    */
+	
+CREATE SYNONYM klienci FOR KICIORB_DEV.clients;
+CREATE SYNONYM adresy FOR KICIORB_DEV.addresses;
+CREATE SYNONYM adres_typ FOR KICIORB_DEV.address_types;
+CREATE SYNONYM ilosc_typ FOR KICIORB_DEV.amount_types;
+CREATE SYNONYM klienci_firma FOR KICIORB_DEV.clients_firm;
+CREATE SYNONYM kontakty FOR KICIORB_DEV.contacts;
+CREATE SYNONYM umowa_typ FOR KICIORB_DEV.contract_types;
+CREATE SYNONYM umowy FOR KICIORB_DEV.contracts;
+CREATE SYNONYM faktury FOR KICIORB_DEV.factures;
+CREATE SYNONYM statusy_zamowien FOR KICIORB_DEV.orders_status;
+CREATE SYNONYM zamowienia FOR KICIORB_DEV.orders;
+CREATE SYNONYM pozycja_faktury FOR KICIORB_DEV.positions;
+CREATE SYNONYM produkt_typ FOR KICIORB_DEV.product_types;
+CREATE SYNONYM produkty FOR KICIORB_DEV.products;
+CREATE SYNONYM uzytkownicy FOR KICIORB_DEV.users;
+CREATE SYNONYM uzytkownicy_firma FOR KICIORB_DEV.users_firm;
+CREATE SYNONYM waluta_typ FOR KICIORB_DEV.value_types;
+CREATE SYNONYM magazyn FOR KICIORB_DEV.warehouse;
+CREATE SYNONYM pracownicy FOR KICIORB_DEV.workers;
